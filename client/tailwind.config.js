cd87e0dec7df360bbcb594225aaa8a559e90301f
K Madhumitha <madhumithakarthikeyan2005@gmail.com>
2025-10-26 21:47:41 +0530

    Safelist icon classes for Tailwind purge

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,5 +7,11 @@ export default {
   theme: {
     extend: {},
   },
+  safelist: [
+    'inline', // Often used by icons
+    'mr-1',   // Used for spacing next to icons in mobile menu
+    // Add other classes icons might need if the above isn't enough
+    // e.g., 'w-6', 'h-6', 'block', text colors if they differ from parent
+  ],
   plugins: [],
 }
